609be9e2f5ed50c27416b83cccbe6dd3caa29fda
HyeonSuShin <hsgod@DESKTOP-8M4C289.localdomain>
2020-10-11 05:32:38 +0900

    Complete AlarmClock

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -89,8 +89,13 @@ void timer_sleep(int64_t ticks)
     int64_t start = timer_ticks();
 
     ASSERT(intr_get_level() == INTR_ON);
-    while (timer_elapsed(start) < ticks)
-        thread_yield();
+
+    thread_sleep(start+ticks);
+
+
+    // Busy Wait부분을 주석 처리.
+    // while (timer_elapsed(start) < ticks)
+    //     thread_yield();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -162,6 +167,8 @@ timer_interrupt(struct intr_frame *args UNUSED)
 {
     ticks++;
     thread_tick();
+    int64_t current_ticks = timer_ticks();
+    thread_awake(current_ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
