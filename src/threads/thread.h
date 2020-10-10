0bece213125ee10c26dafbc8cc39afde72bf4317
HyeonSuShin <hsgod@DESKTOP-8M4C289.localdomain>
2020-10-11 05:47:24 +0900

    Complete AlarmClock

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,7 +89,6 @@ struct thread
     uint8_t *stack;            /* Saved stack pointer. */
     int priority;              /* Priority. */
     struct list_elem allelem;  /* List element for all threads list. */
-    int64_t wakeup_tick; // 수정
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem; /* List element. */
